0464e5503dc6afee1506e781173f581e76c2fc6a
JeeSooPark <76983198+BeTheWings@users.noreply.github.com>
2024-09-07 14:33:35 +0900

    App.js수정본
    수정

--- wanted/src/main/front/src/pages/index/App.js
+++ wanted/src/main/front/src/pages/index/App.js
@@ -4,6 +4,7 @@ import About from "./about/About";
 import BookList from "./book/BookList";
 import BookInfo from "./book/BookInfo";
 import EventList from "./event/EventList";
+import EventInfo from "./event/EventInfo";
 import NoticeList from "./serviceCenter/notice/NoticeList";
 import NoticeDetail from "./serviceCenter/notice/NoticeDetail";
 import QnaList from "./serviceCenter/qna/QnaList";
@@ -43,6 +44,42 @@ function App() {
             <Footer />
         </div>
     );
+=======
+import CustomerCenter from "./serviceCenter/qna/CustomerCenter"
+
+import Main from "./Main";
+import React from "react";
+
+
+function App() {
+    useEffect(()=>{
+        const csrfTokenMeta = document.querySelector("meta[name='_csrf']");
+        console.log("Token "+csrfTokenMeta);
+        if(csrfTokenMeta){
+            axios.defaults.headers.common["X-XSRF-TOKEN"]=csrfTokenMeta.content;
+        }
+    },[]);
+
+  return (
+      <Routes>
+          <Route path="/" element={<Main/>}></Route>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/bookList" element={<BookList/>}></Route>
+          <Route path="/bookInfo/:bookId" element={<BookInfo/>}></Route>
+          <Route path="/events" element={<EventList/>}></Route>
+          <Route path="/events/:eventId" element={<EventInfoDetail/>}></Route>
+          <Route path="/noticeList" element={<NoticeList/>}></Route>
+          <Route path="/noticeDetail/:noticeId" element={<NoticeDetail/>}></Route>
+          <Route path="/questionAndAnswer" element={<QnaList/>}></Route>
+          <Route path="/qnaContent" element={<QnaContent/>}></Route>
+          <Route path="/login" element={<Login/>}></Route>
+          <Route path="/customerCenter" element={<CustomerCenter/>}></Route>
+          <Route path="/signIn" element={<SignIn/>}></Route>
+          <Route path="/signUp" element={<SignUp/>}></Route>
+      </Routes>
+  );
+
+>>>>>>> Stashed changes
 }
 
 export default App;
