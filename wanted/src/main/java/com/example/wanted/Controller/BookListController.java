e05001e5b35332ef0c0cf33c5ceb6c18f1c6fe1c
gbk1414 <gbk1414@gmail.com>
2024-10-03 10:08:17 +0900

    feat(BookListController.java): 관리자 기능 추가(책관리)
    컨트롤러에 메서드별 주석 추가

--- wanted/src/main/java/com/example/wanted/Controller/BookListController.java
+++ wanted/src/main/java/com/example/wanted/Controller/BookListController.java
@@ -17,30 +17,35 @@ public class BookListController {
     private static final Logger logger = LoggerFactory.getLogger(BookListController.class);
     private final BookListService bookListService;
 
+    // 책 목록 조회 - 관리자, 유저 둘다
     @GetMapping
     public List<BookInfoDto> getBookList() {
         logger.info("Fetching all books");
         return bookListService.findAllBookInfo();
     }
 
+    // 특정 도서 정보를 조회 - 관리자, 유저 둘다
     @GetMapping("/{bookId}")
     public BookInfoDto getBookInfo(@PathVariable Long bookId) {
         logger.info("Fetching book with ID: {}", bookId);
         return bookListService.findOneBookInfo(bookId);
     }
 
+    // 새로운 도서 추가 - 관리자용
     @PostMapping
     public BookInfoDto createBook(@RequestBody BookInfoDto bookInfoDto) {
         logger.info("Creating new book");
         return bookListService.saveBookInfo(bookInfoDto);
     }
 
+    // 기존 도서 정보 수정 - 관리자용
     @PutMapping("/{bookId}")
     public BookInfoDto updateBook(@PathVariable Long bookId, @RequestBody BookInfoDto bookInfoDto) {
         logger.info("Updating book with ID: {}", bookId);
         return bookListService.updateBookInfo(bookId, bookInfoDto);
     }
 
+    // 특정 도서 삭제 - 관리자용
     @DeleteMapping("/{bookId}")
     public void deleteBook(@PathVariable Long bookId) {
         logger.info("Deleting book with ID: {}", bookId);
