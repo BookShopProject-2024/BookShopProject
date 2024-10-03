2b33433eb197ce6805b8106cad766c1b45fc2431
gbk1414 <gbk1414@gmail.com>
2024-10-03 11:35:41 +0900

    수정 1

--- wanted/src/main/java/com/example/wanted/Controller/BookListController.java
+++ wanted/src/main/java/com/example/wanted/Controller/BookListController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/info/books")
+@RequestMapping("/info/bookList")
 @RequiredArgsConstructor
 public class BookListController {
 
